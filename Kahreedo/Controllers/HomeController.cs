ebbba3df1b307bb9e70dd7e0c65cd0063ec65bb7
Rahimeen Altaf <143562060+Rahimeen-Altaf@users.noreply.github.com>
2024-05-21 02:49:12 +0500

    complete

---
+++ Kahreedo/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Khareedo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Khareedo.Controllers
+{
+    public class HomeController : Controller
+    {
+        KhareedoEntities db = new KhareedoEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.MenProduct = db.Products.Where(x => x.Category.Name.Equals("Ferodo")).ToList();
+            ViewBag.WomenProduct = db.Products.Where(x => x.Category.Name.Equals("Tires & Tubes")).ToList();
+            ViewBag.SportsProduct = db.Products.Where(x => x.Category.Name.Equals("misc")).ToList();
+            ViewBag.ElectronicsProduct = db.Products.Where(x => x.Category.Name.Equals("Handlebar Accesories")).ToList();
+            ViewBag.Slider = db.genMainSliders.ToList();
+            ViewBag.PromoRight = db.genPromoRights.ToList();
+
+            this.GetDefaultData();
+
+
+
+            return View();
+        }      
+
+    }
+}
---
+++ Kahreedo/Models/TemData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Khareedo.Models
+{
+    public static class TemData
+    {
+        public static int EmpID { get; set; }
+    }
+}
